6775dedf14c84d01febcfbb1f8b958f8acb21f20
BorenTsai <35507350+BorenTsai@users.noreply.github.com>
2018-04-24 11:45:49 -0700

    first java function

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,13 @@
 $(document).ready(function() {
-	$
+	$('.scrollTo').click(function(){
+		var getElement = $(this).attr('href');
+		(console.log(getElement.length))
+		if ($(getElement).length){
+			var getOffset = $(getElement).offset().top;
+			$('html,body').animate({
+				scrollTop: getOffset
+			}, 500);
+		}
+		return false
+	})
 });
