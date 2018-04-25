1b32a56b219f09d9c1ffd31335036bddaca3ffbf
BorenTsai <35507350+BorenTsai@users.noreply.github.com>
2018-04-24 18:47:35 -0700

    website fade-in

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,14 @@
 $(document).ready(function() {
-	// jQuery Function Number 1 (click)
+	// jQuery Fucntion Number 1 (removeClass)
+	$(function() {
+		$('body').removeClass('fade-out');
+	})
+	// jQuery Function Number 2 (click)
 	$('.scrollTo').click(function(){
-		// jQuery Function Number 2 (attr)
+		// jQuery Function Number 3 (attr)
 		var getElement = $(this).attr('href');
 		if ($(getElement).length){
-			// jQuery Function Number 3 (offset)
+			// jQuery Function Number 4 (offset)
 			var getOffset = $(getElement).offset().top;
 			$('html,body').animate({
 				scrollTop: getOffset
@@ -16,7 +20,7 @@ $(document).ready(function() {
 	$(window).scroll(function() {
 		$('.pic').each( function(i) {
 			var bottom_of_object = $(this).offset().top + $(this).outerHeight();
-			// jQuery Function Number 4 (scrollTop)
+			// jQuery Function Number 5 (scrollTop)
 			var bottom_of_window = $(window).scrollTop() + $(window).height();
 			if (bottom_of_window > bottom_of_object) {
 				$(this).animate({'opacity': '1'},1000);
