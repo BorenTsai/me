66bcf68bdd06f61abf4169ba9f6f7634e7d83c5d
BorenTsai <35507350+BorenTsai@users.noreply.github.com>
2018-04-24 17:15:00 -0700

    portfolio section animation

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,10 @@
 $(document).ready(function() {
+	// jQuery Function Number 1 (click)
 	$('.scrollTo').click(function(){
+		// jQuery Function Number 2 (attr)
 		var getElement = $(this).attr('href');
-		(console.log(getElement.length))
 		if ($(getElement).length){
+			// jQuery Function Number 3 (offset)
 			var getOffset = $(getElement).offset().top;
 			$('html,body').animate({
 				scrollTop: getOffset
@@ -10,4 +12,15 @@ $(document).ready(function() {
 		}
 		return false
 	})
+	
+	$(window).scroll(function() {
+		$('.pic').each( function(i) {
+			var bottom_of_object = $(this).offset().top + $(this).outerHeight();
+			// jQuery Function Number 4 (scrollTop)
+			var bottom_of_window = $(window).scrollTop() + $(window).height();
+			if (bottom_of_window > bottom_of_object) {
+				$(this).animate({'opacity': '1'},1000);
+			}
+		});
+	});
 });
