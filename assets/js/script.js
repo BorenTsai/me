d342b3d0f7dbd50884d86f8baba3657b1b636641
BorenTsai <35507350+BorenTsai@users.noreply.github.com>
2018-08-20 17:45:16 -0700

    added script effects

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,14 +1,19 @@
 $(document).ready(function() {
-	// jQuery Fucntion Number 1 (removeClass)
+	let pageHeight = $(window).height();
+	let distanceFromTop = 0;
+	$(window).scroll(function() {
+		distanceFromTop = $(window).scrollTop();
+		if (distanceFromTop == 0) {
+			$(".hero_image").css("transform", "scale(" + 1 + ")");
+		}
+  		$(".hero_image").css("transform", 'scale(' + (1 + (distanceFromTop/pageHeight)) + ')');
+  	});
 	$(function() {
 		$('body').removeClass('fade-out');
 	})
-	// jQuery Function Number 2 (click)
 	$('.scrollTo').click(function(){
-		// jQuery Function Number 3 (attr)
 		var getElement = $(this).attr('href');
 		if ($(getElement).length){
-			// jQuery Function Number 4 (offset)
 			var getOffset = $(getElement).offset().top;
 			$('html,body').animate({
 				scrollTop: getOffset
@@ -20,7 +25,6 @@ $(document).ready(function() {
 	$(window).scroll(function() {
 		$('.pic').each( function(i) {
 			var bottom_of_object = $(this).offset().top + $(this).outerHeight();
-			// jQuery Function Number 5 (scrollTop)
 			var bottom_of_window = $(window).scrollTop() + $(window).height();
 			if (bottom_of_window > bottom_of_object) {
 				$(this).animate({'opacity': '1'},1000);
@@ -28,7 +32,6 @@ $(document).ready(function() {
 		});
 	});
 	
-	//jQuery Function Number 6(hover)
 	$('.icon').hover(function () {
 		$(this).css('transform', 'scale(' + (1.05) + ')');
 	}, function() {
